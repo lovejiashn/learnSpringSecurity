94c272ea484171abe5f2f1e161c37689c399b2dd
jiangjs <421582841@qq.com>
2022-03-19 19:35:19 +0800

    [新增]学习SpringSecurity

---
+++ src/main/java/com/jiashn/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,16 @@
+package com.jiashn.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangjs
+ */
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
+
+}
